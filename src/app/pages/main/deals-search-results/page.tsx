d41fe8ed9d66603ae5c7f05a36ee3e619dd5e38b
aaliimran <alikubanov@gmail.com>
2024-08-10 18:24:04 +0600

    fix problem in deals-search-results

--- src/app/pages/main/deals-search-results/page.tsx
+++ src/app/pages/main/deals-search-results/page.tsx
@@ -1,95 +1,95 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-interface Deal {
-  ID: number;
-  "Название сделки": string;
-  "Контакт: Рабочий телефон": string;
-  "Стадия сделки": string;
-  "Номер договора": string;
-  "Дата начала": string;
-}
+// interface Deal {
+//   ID: number;
+//   "Название сделки": string;
+//   "Контакт: Рабочий телефон": string;
+//   "Стадия сделки": string;
+//   "Номер договора": string;
+//   "Дата начала": string;
+// }
 
-const Deals = () => {
-  const [selectedData, setSelectedData] = useState<Deal | null>(null);
-  const [searchTerm, setSearchTerm] = useState<string>("");
+// const Deals = () => {
+//   const [selectedData, setSelectedData] = useState<Deal | null>(null);
+//   const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const handleSearch = async () => {
-    if (searchTerm.trim() === "") {
-      setSelectedData(null);
-      return;
-    }
+//   const handleSearch = async () => {
+//     if (searchTerm.trim() === "") {
+//       setSelectedData(null);
+//       return;
+//     }
 
-    try {
-      const [docsResponse, consulateResponse] = await Promise.all([
-        fetch("/deals/sasl_docs.json"),
-        fetch("/deals/sasl_consulate.json"),
-      ]);
+//     try {
+//       const [docsResponse, consulateResponse] = await Promise.all([
+//         fetch("/deals/sasl_docs.json"),
+//         fetch("/deals/sasl_consulate.json"),
+//       ]);
 
-      const docsData: Deal[] = await docsResponse.json();
-      const consulateData: Deal[] = await consulateResponse.json();
+//       const docsData: Deal[] = await docsResponse.json();
+//       const consulateData: Deal[] = await consulateResponse.json();
 
-      const combinedData = [...docsData, ...consulateData];
-      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+//       const combinedData = [...docsData, ...consulateData];
+//       const lowerCaseSearchTerm = searchTerm.toLowerCase();
 
-      const firstMatch = combinedData.find((deal) =>
-        Object.values(deal).some((value) =>
-          value.toString().toLowerCase().includes(lowerCaseSearchTerm)
-        )
-      );
+//       const firstMatch = combinedData.find((deal) =>
+//         Object.values(deal).some((value) =>
+//           value.toString().toLowerCase().includes(lowerCaseSearchTerm)
+//         )
+//       );
 
-      setSelectedData(firstMatch || null);
-    } catch (error) {
-      console.error("Ошибка при загрузке данных:", error);
-    }
-  };
+//       setSelectedData(firstMatch || null);
+//     } catch (error) {
+//       console.error("Ошибка при загрузке данных:", error);
+//     }
+//   };
 
-  return (
-    <div className="flex flex-col justify-start items-start p-0 pb-20 gap-6">
-      <h1 className="text-[#000000] text-[32px] font-title font-bold">
-        Проверка статуса соискателя
-      </h1>
+//   return (
+//     <div className="flex flex-col justify-start items-start p-0 pb-20 gap-6">
+//       <h1 className="text-[#000000] text-[32px] font-title font-bold">
+//         Проверка статуса соискателя
+//       </h1>
 
-      <div className="flex gap-2 mb-4 w-[100%]">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="p-4 border border-color-#F3F4F6 rounded-xl text-[#000000] font-title font-semibold w-[90%]"
-          placeholder="ФИО / Номер договора / Номер телефона / ID соискат"
-        />
-        <button
-          onClick={handleSearch}
-          className="px-4 py-2 bg-[#1830FF] text-[#FFFFFF] text-[16px] font-title font-bold rounded-xl"
-        >
-          поиск
-        </button>
-      </div>
+//       <div className="flex gap-2 mb-4 w-[100%]">
+//         <input
+//           type="text"
+//           value={searchTerm}
+//           onChange={(e) => setSearchTerm(e.target.value)}
+//           className="p-4 border border-color-#F3F4F6 rounded-xl text-[#000000] font-title font-semibold w-[90%]"
+//           placeholder="ФИО / Номер договора / Номер телефона / ID соискат"
+//         />
+//         <button
+//           onClick={handleSearch}
+//           className="px-4 py-2 bg-[#1830FF] text-[#FFFFFF] text-[16px] font-title font-bold rounded-xl"
+//         >
+//           поиск
+//         </button>
+//       </div>
 
-      <div className="flex flex-col  p-6 gap-518px w-[865px]">
-        <ul className="flex flex-col gap-3">
-          {selectedData ? (
-            <li
-              className="text-[20px] font-title flex flex-col gap-3"
-              key={selectedData.ID}
-            >
-              <p className="text-[#000000] text-[24px] font-title font-bold">
-                {selectedData["Название сделки"]}
-              </p>
-              <div>
-                <p>
-                  Контакты соискателя:{" "}
-                  {selectedData["Контакт: Рабочий телефон"]}
-                </p>
-                <p>Статус соискателя: {selectedData["Стадия сделки"]}</p>
-                <p>Номер договора: {selectedData["Номер договора"]}</p>
-                <p>Дата начала: {selectedData["Дата начала"]}</p>
-              </div>
-            </li>
-          ) : null}
-        </ul>
-      </div>
-    </div>
-  );
-};
+//       <div className="flex flex-col  p-6 gap-518px w-[865px]">
+//         <ul className="flex flex-col gap-3">
+//           {selectedData ? (
+//             <li
+//               className="text-[20px] font-title flex flex-col gap-3"
+//               key={selectedData.ID}
+//             >
+//               <p className="text-[#000000] text-[24px] font-title font-bold">
+//                 {selectedData["Название сделки"]}
+//               </p>
+//               <div>
+//                 <p>
+//                   Контакты соискателя:{" "}
+//                   {selectedData["Контакт: Рабочий телефон"]}
+//                 </p>
+//                 <p>Статус соискателя: {selectedData["Стадия сделки"]}</p>
+//                 <p>Номер договора: {selectedData["Номер договора"]}</p>
+//                 <p>Дата начала: {selectedData["Дата начала"]}</p>
+//               </div>
+//             </li>
+//           ) : null}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Deals;
+// export default Deals;
